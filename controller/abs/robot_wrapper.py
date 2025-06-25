c7a80d57c2f9ff41df50f96563af941eae14dabc
ChristianMariano4 <christian.mariano@mail.polimi.it>
2025-06-25 13:25:52 +0200

    chore: added some flags in robot wrapper

--- controller/abs/robot_wrapper.py
+++ controller/abs/robot_wrapper.py
@@ -23,6 +23,11 @@ class RobotWrapper(ABC):
         self.graph_manager = graph_manager
         self.move_enable = move_enable
 
+
+    def __init__(self, move_enable: bool = False):
+        self.move_enable = move_enable
+        self.gesture_trajectory_mapping = {}
+
     @abstractmethod
     def connect(self):
         pass
