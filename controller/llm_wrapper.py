aa97745366a05912e64aa45ed96328299909a392
Christian Mariano <christianmariano444@gmail.com>
2025-07-23 10:50:03 +0200

    chore: expand llm_wrapper to be used for more goals

--- controller/llm_wrapper.py
+++ controller/llm_wrapper.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import os
 import openai
 from openai import Stream, ChatCompletion
@@ -8,6 +9,11 @@ LLAMA3 = "meta-llama/Meta-Llama-3-8B-Instruct"
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 chat_log_path = os.path.join(CURRENT_DIR, "assets/chat_log.txt")
+
+class RequestType(Enum):
+    EXPLORE_DIRECTION = "explore_direction"
+    SIMPLE = "simple"
+    SINGLE_IMAGE = "single_image"
 
 class LLMWrapper:
     def __init__(self, temperature=0.0):
@@ -21,26 +27,55 @@ class LLMWrapper:
             api_key=os.environ.get("OPENAI_API_KEY"),
         )
 
-    def request(self, prompt, image=None, model_name=GPT4, stream=False) -> str | Stream[ChatCompletion.ChatCompletionChunk]:
+    def request(self, prompt, image=None, images=None, model_name=GPT4, stream=False, request_type: RequestType = RequestType.SIMPLE) -> str | Stream[ChatCompletion.ChatCompletionChunk]:
         if model_name == LLAMA3:
             client = self.llama_client
         else:
             client = self.gpt_client
         
-        if image is not None:
-            response = client.chat.completions.create(
-                model=model_name,
-                messages=[{"role": "user", "content": [{"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image}"}}, prompt]}],
-                temperature=self.temperature,
-                stream=stream,
-            )
-        else:
-            response = client.chat.completions.create(
-                model=model_name,
-                messages=[{"role": "user", "content": prompt}],
-                temperature=self.temperature,
-                stream=stream,
-            )
+        match request_type:
+            case RequestType.SIMPLE:
+                response = client.chat.completions.create(
+                    model=model_name,
+                    messages=[{"role": "user", "content": prompt}],
+                    temperature=self.temperature,
+                    stream=stream,
+                )
+            
+            case RequestType.SINGLE_IMAGE:
+                assert image is not None, f"Image not given in a {RequestType.SINGLE_IMAGE} request"
+                response = client.chat.completions.create(
+                    model=model_name,
+                    messages=[{"role": "user", "content": [{"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image}"}}, prompt]}],
+                    temperature=self.temperature,
+                    stream=stream,
+                )
+            
+            case RequestType.EXPLORE_DIRECTION:
+                assert images is not None, f"Images not given in a {RequestType.SINGLE_IMAGE} request"
+                response = client.chat.completions.create(
+                    model=model_name,
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": [
+                                {"type": "text", "text": prompt},
+                                {"type": "image_url", "image_url": {"url": "data:image/jpeg;base64," + image[0], "name": "north.jpg"}},
+                                {"type": "image_url", "image_url": {"url": "data:image/jpeg;base64," + image[1], "name": "north-east.jpg"}},
+                                {"type": "image_url", "image_url": {"url": "data:image/jpeg;base64," + image[2], "name": "east.jpg"}},
+                                {"type": "image_url", "image_url": {"url": "data:image/jpeg;base64," + image[3], "name": "south-east.jpg"}},
+                                {"type": "image_url", "image_url": {"url": "data:image/jpeg;base64," + image[4], "name": "south.jpg"}},
+                                {"type": "image_url", "image_url": {"url": "data:image/jpeg;base64," + image[5], "name": "south-west.jpg"}},
+                                {"type": "image_url", "image_url": {"url": "data:image/jpeg;base64," + image[6], "name": "west.jpg"}},
+                                {"type": "image_url", "image_url": {"url": "data:image/jpeg;base64," + image[7], "name": "north-west.jpg"}},
+                            ]
+                        }
+                    ],
+                    temperature=self.temperature,
+                    stream=stream,
+                    response_format={"type": "json_object"} 
+                )
+
 
         # save the message in a txt
         with open(chat_log_path, "a") as f:
