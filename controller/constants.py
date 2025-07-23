6e9912f51271ac57858317f5acda598e3bc2250e
Christian Mariano <christianmariano444@gmail.com>
2025-07-23 12:16:50 +0200

    chore: modify some constants

--- controller/constants.py
+++ controller/constants.py
@@ -1,6 +1,6 @@
-REGION_THRESHOLD: int = 50 # how distant in centimeters are the regions
-X_BOUND: int = 150
-Y_BOUND: int = 150
+REGION_THRESHOLD: int = 100 # how distant in centimeters are the regions
+X_BOUND: int = 50
+Y_BOUND: int = 50
 
 # Used paths
 HIGH_LEVEL_SKILL_FILE = "controller/assets/tello/high_level_skills.json"
