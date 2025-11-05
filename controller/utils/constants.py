245503a0a978e9450b5e9ec39f5fdf3165ec9031
Christian Mariano <christianmariano444@gmail.com>
2025-11-05 15:58:31 +0100

    chore: add a constant for flyzone image path

--- controller/utils/constants.py
+++ controller/utils/constants.py
@@ -3,8 +3,9 @@ Y_BOUND: int = 50
 REGION_THRESHOLD: int = 100 # How distant in centimeters are the regions. After that distance, a new region is automatically created
 
 # Used paths
-HIGH_LEVEL_SKILL_FILE = "controller/assets/tello/skills/high_level_skills.json"
-FLYZONE_TXT = "controller/assets/tello/flyzone/flyzone.txt"
+HIGH_LEVEL_SKILL_PATH = "controller/assets/tello/skills/high_level_skills.json"
+FLYZONE_TXT_PATH = "controller/assets/tello/flyzone/flyzone.txt"
+FLYZONE_USER_IMAGE_PATH = "controller/assets/tello/temp/user_flyzone.jpg"
 
 # Robot specific name
 ROBOT_NAME = 'tello'
