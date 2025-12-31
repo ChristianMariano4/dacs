16e5c742be78a79c4155548cfcc07baf1ccd4828
Christian Mariano <christianmariano444@gmail.com>
2025-12-31 16:21:43 +0100

    refactor: delete comments

--- controller/middle_layer/middle_layer.py
+++ controller/middle_layer/middle_layer.py
@@ -13,38 +13,6 @@ class MiddleLayer:
             self.flyzone = f.read()
         # self.username = ""
 
-        ## U-shaped flyzone
-        # self.flyzone_polygons = [
-        #     # Left vertical leg of the U
-        #     Polygon([
-        #         (0, 0),
-        #         (0, 100),
-        #         (30, 100),
-        #         (30, 0)
-        #     ]),
-            
-        #     # Right vertical leg of the U
-        #     Polygon([
-        #         (70, 0),
-        #         (70, 100),
-        #         (100, 100),
-        #         (100, 0)
-        #     ]),
-            
-        #     # Top horizontal bar of the U
-        #     Polygon([
-        #         (30, 70),
-        #         (30, 100),
-        #         (70, 100),
-        #         (70, 70)
-        #     ])
-        # ]
-
-        ## Cirle flyzone
-        # circle = Point(0, 0).buffer(25, resolution=32)  # resolution controls smoothness
-        # # resolution=32 creates 128 boundary points (4 × resolution) — higher resolution means smoother approximation.
-        # self.flyzone_polygons = [Polygon(list(circle.exterior.coords))]
-
     @classmethod
     def _parse_flyzone(cls, file_path):
         polygons = []
